55f8ebb07aa7c8414f9eddf30e151c5cf0c86496
prashant-rajpurohit <prashant.rajpurohit@paktolus.com>
2026-05-06 23:12:59 +0530

    Setting up the theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import Providers from "@/providers";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,11 +27,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geistSans.variable)}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
---
+++ src/providers/theme-provider.tsx
@@ -0,0 +1,9 @@
+import { ThemeProvider as NextThemeProvider } from "next-themes";
+export function ThemeProvider({
+  children,
+  ...props
+}: React.PropsWithChildren<
+  React.ComponentProps<typeof NextThemeProvider>
+>) {
+  return <NextThemeProvider {...props}>{children}</NextThemeProvider>;
+}
